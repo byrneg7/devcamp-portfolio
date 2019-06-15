dec36b716cfbb0e184212628b6f89369edf17156
Greg <you@example.com>
2019-06-15 22:35:27 +0100

    added edit buttons to portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -10,11 +10,11 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :portfolio))
-
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    p "params****", params.inspect, "params****"
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully created.' }
+        format.html { redirect_to portfolios_path, notice: "Portfolio item was successfully created." }
       else
         format.html { render :new }
       end
@@ -22,18 +22,17 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-      @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def update
     @portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :portfolio))
-        format.html { redirect_to portfolios_path, notice: 'Record was successfully updated.' }
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :bpdy))
+        format.html { redirect_to portfolios_path, notice: "Record was successfully updated." }
       else
         format.html { render :edit }
       end
     end
   end
-
 end
