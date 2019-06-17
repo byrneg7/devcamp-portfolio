026184d2f611501e91325b2183bd47a58dc21d00
Greg Byrne <byrneg7@tcd.ie>
2019-06-17 22:29:05 +0100

    cutsomized portfolio routes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -11,7 +11,6 @@ class PortfoliosController < ApplicationController
 
   def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
-    p "params****", params.inspect, "params****"
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: "Portfolio item was successfully created." }
@@ -34,6 +33,10 @@ class PortfoliosController < ApplicationController
         format.html { render :edit }
       end
     end
+  end
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  resources :portfolios
   root to: 'pages#home'
-  
+
+  resources :portfolios, except: [:show]  
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
+
   get 'about', to: 'pages#about' # same as {:to => 'pages#about'}
   get 'contact', to: 'pages#contact'
+  
   resources :blogs
+
 end
