754e8a1571015b12fabb33147831dbc825bc5073
Greg Byrne <byrneg7@tcd.ie>
2019-06-19 23:09:40 +0100

    implemented enum and friendly urls. added toggle for draft and published blogs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
 
   resources :portfolios, except: [:show]  
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
@@ -8,6 +13,5 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about' # same as {:to => 'pages#about'}
   get 'contact', to: 'pages#contact'
   
-  resources :blogs
 
 end
