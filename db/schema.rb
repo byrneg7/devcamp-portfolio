2c34c6a59767955522cb6bd8b8a28a209b7bc784
Greg Byrne <byrneg7@tcd.ie>
2019-06-25 00:07:52 +0100

    added concerns for portfolio and skill helper

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_23_213357) do
+ActiveRecord::Schema.define(version: 2019_06_24_224537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2019_06_23_213357) do
     t.integer "percent"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
