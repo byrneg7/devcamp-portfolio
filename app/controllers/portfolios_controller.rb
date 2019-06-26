4adb753eb145d4460992cf441194415917223db5
Greg Byrne <byrneg7@tcd.ie>
2019-06-27 00:03:26 +0100

    implemented technologies sub-documents in portfolio controller and views

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -11,13 +11,16 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
+
+    3.times { @portfolio_item.technologies.build }
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body,
+                                                                      technologies_attributes: [:name]))
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: "Portfolio item was successfully created." }
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully created.' }
       else
         format.html { render :new }
       end
@@ -32,7 +35,7 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :bpdy))
-        format.html { redirect_to portfolios_path, notice: "Record was successfully updated." }
+        format.html { redirect_to portfolios_path, notice: 'Record was successfully updated.' }
       else
         format.html { render :edit }
       end
